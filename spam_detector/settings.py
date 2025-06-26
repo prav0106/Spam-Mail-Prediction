a026b70a5f98807cd7eb66e982b2ae0f6ec34e56
Pravleen Kaur <kaurpravleen12@gmail.com>
2025-06-26 23:54:57 +0530

    Fix for production static and debug settings

--- spam_detector/settings.py
+++ spam_detector/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ek9z&h=h&ctqu1kyzgvw6c!glo&$h!sb8*tfz9l^-p+9)g8c@q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
