819d37d28efd1ca128058c13992042c0c4f8e91b
Pravleen Kaur <kaurpravleen12@gmail.com>
2025-06-27 12:13:57 +0530

    Fix: CSRF trusted origin for deployment

--- spam_detector/settings.py
+++ spam_detector/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://spam-mail-prediction.up.railway.app',
+]
